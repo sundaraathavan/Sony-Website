f85be47915c1fd51a07c66086b7b1b591aa48914
athavan <52910050+sundaraathavan@users.noreply.github.com>
2022-05-02 21:05:34 -0700

    main

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
+import { AiFillFacebook ,AiFillInstagram, AiOutlineTwitter, AiFillLinkedin } from 'react-icons/ai';
 
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 JSM Headphones All rights reserverd</p>
+      <p>All rights reserverd sundaraathavan@gmail.com 2022</p>
       <p className="icons">
+        <AiFillFacebook />
         <AiFillInstagram />
         <AiOutlineTwitter />
+        <AiFillLinkedin />
       </p>
     </div>
   )
